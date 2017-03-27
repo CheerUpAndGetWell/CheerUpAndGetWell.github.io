6b7d6cfb8a6fd7dbdd37cbd3ea46c5a9d0967f08
Zach Mazzie <zachmazzie@gmail.com>
2017-03-26 18:17:08 -0600

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Test 1</title>
+</head>
+
+
+<body>
+<form action="test2.php" method="post">
+	<ul>
+    	<li>
+        	<label for="name">Message</label>
+            <input type="text" id="msg" name="msg"/>
+        </li>
+        <li>
+        	<input type="submit" name="sub" value="Send"/>
+        </li>
+    </ul>
+</form>
+</body>
+</html>
